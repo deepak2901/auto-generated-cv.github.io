bf74a3764724a3ecd3656445499a826af8795f48
deepak <deepak.kondapalli29@gmail.com>
2023-02-03 22:49:31 +0530

    linking html js progress

--- app.js
+++ app.js
@@ -3,15 +3,76 @@ form.addEventListener("submit", handleSubmit);
 
 // import all the fields from the form.
 // eg.
-// const name = document.getElementById("name");
+const name = document.getElementById("name");
+const tel = document.getElementById("tel");
 
 function handleSubmit(e) {
   e.preventDefault();
-  //   here get the values from the form and set it to the resume
-  // eg.
-  //   const name_resume = document.getElementById("name_resume");
-  //   name_resume.innerHTML = name.value;
-  //   similarly for all the other fields
+  // //   here get the values from the form and set it to the resume
+
+  // form name, email, phone, github, linkedin
+
+
+    var name = document.getElementById("name").value;
+    document.getElementById("name_resume").innerHTML = name;
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email;
+
+    var tel = document.getElementById("tel").value;
+    document.getElementById("phone_num").innerHTML = tel;
+
+    var github = document.getElementById("github").value;
+    document.getElementById("git_hub").innerHTML = github
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+    var email = document.getElementById("email").value;
+    document.getElementById("email_id").innerHTML = email
+
+
+    document.getElementById("name").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("github").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+    document.getElementById("tel").value = '';
+
+  // //   similarly for all the other fields  
 }
 
 function handleLeft() {
